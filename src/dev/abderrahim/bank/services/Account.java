42ea218904e5e1eeccf01ae5f431bc22db138c86
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-26 15:49:54 +0100

    Add transactions history to Account class

--- src/dev/abderrahim/bank/services/Account.java
+++ src/dev/abderrahim/bank/services/Account.java
@@ -1,14 +1,24 @@
 package dev.abderrahim.bank.services;
 
+import java.time.ZonedDateTime;
+import java.util.LinkedList;
+import java.util.SequencedCollection;
 import java.util.concurrent.ThreadLocalRandom;
+
+import dev.abderrahim.bank.models.Transaction;
+import dev.abderrahim.bank.types.TransactionType;
 
 public class Account implements AccountService {
     private final long id;
     private int balance;
+    // Since transaction history must be kept ordered and to reduce coupling on an actual implementation,
+    // I chose to use a SequencedCollection as data type of transactions history
+    private final SequencedCollection<Transaction> transactionsHistory;
 
     public Account() {
         this.id = ThreadLocalRandom.current().nextLong(1, Long.MAX_VALUE);
         this.balance = 0;
+        this.transactionsHistory = new LinkedList<>();
     }
 
     @Override
@@ -17,6 +27,7 @@ public class Account implements AccountService {
             throw new IllegalArgumentException("Cannot deposit a negative amount.");
 
         this.balance += amount;
+        logTransaction(amount, TransactionType.DEPOSIT);
     }
 
     @Override
@@ -28,10 +39,16 @@ public class Account implements AccountService {
             throw new IllegalArgumentException("The amount is greater than the current balance");
 
         this.balance -= amount;
+        logTransaction(amount, TransactionType.WITHDRAW);
     }
 
     @Override
     public void printStatement() {
         System.out.println("Date\t\t||\tAmount\t||\tBalance");
+        transactionsHistory.forEach(System.out::println);
+    }
+    private void logTransaction(int amount, TransactionType type) {
+        Transaction t = new Transaction(amount, type, ZonedDateTime.now().toInstant(), balance);
+        transactionsHistory.addFirst(t);
     }
 }
