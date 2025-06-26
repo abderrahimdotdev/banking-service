ad6f5df4a6ca6f73ac66f964d165c45afbbb86fb
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-26 15:31:46 +0100

    Add implementation of AccountService methods to Account class

--- src/dev/abderrahim/bank/services/Account.java
+++ src/dev/abderrahim/bank/services/Account.java
@@ -1,23 +1,37 @@
 package dev.abderrahim.bank.services;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Account implements AccountService {
+    private final long id;
+    private int balance;
+
+    public Account() {
+        this.id = ThreadLocalRandom.current().nextLong(1, Long.MAX_VALUE);
+        this.balance = 0;
+    }
 
     @Override
     public void deposit(int amount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+        if (amount <= 0)
+            throw new IllegalArgumentException("Cannot deposit a negative amount.");
+
+        this.balance += amount;
     }
 
     @Override
     public void withdraw(int amount) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+
+        if (amount <= 0)
+            throw new IllegalArgumentException("Withdrawn amount cannot be zero or a negative number.");
+        else if (balance != 0 && amount > balance)
+            throw new IllegalArgumentException("The amount is greater than the current balance");
+
+        this.balance -= amount;
     }
 
     @Override
     public void printStatement() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'printStatement'");
+        System.out.println("Date\t\t||\tAmount\t||\tBalance");
     }
-    
 }
