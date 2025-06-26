9135aba3f83893bb2f69c138d5120ae192ec684a
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-26 15:51:06 +0100

    Add sample code to main() method

--- src/dev/abderrahim/bank/Main.java
+++ src/dev/abderrahim/bank/Main.java
@@ -1,7 +1,17 @@
 package dev.abderrahim.bank;
 
+import dev.abderrahim.bank.services.Account;
+import dev.abderrahim.bank.services.AccountService;
+
 public class Main {
     public static void main(String[] args) throws Exception {
+        
+        AccountService acc = new Account();
 
+        acc.deposit(1000);
+        acc.deposit(2000);
+        acc.withdraw(500);
+
+        acc.printStatement();
     }
 }
