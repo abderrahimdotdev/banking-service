0ed77248d345821d461b39b7d2cddd50e193c884
Abderrahim <abderrahimdotdev@gmail.com>
2025-06-26 15:28:49 +0100

    Add Account class

---
+++ src/dev/abderrahim/bank/services/Account.java
@@ -0,0 +1,23 @@
+package dev.abderrahim.bank.services;
+
+public class Account implements AccountService {
+
+    @Override
+    public void deposit(int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+    }
+
+    @Override
+    public void withdraw(int amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+    }
+
+    @Override
+    public void printStatement() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'printStatement'");
+    }
+    
+}
